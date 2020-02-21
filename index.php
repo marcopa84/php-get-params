9663b80771c6d3662435f8620107e44ee805c4fb
Marco <marcopa84@hotmail.it>
2020-02-21 16:04:06 +0100

    first exercise

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+  $password = $_GET['password'];
+
+  if ($password == 'boolean') {
+    $message = 'BENVENUTO! PASSWORD CORRETTA!';
+    $class = 'green';
+  }
+  else {
+    $message = 'ACCESSO NON CONSENTITO! PASSWORD ERRATA!';
+    $class = 'red';
+  }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <style media="screen">
+      .red{
+        color: red;
+      }
+      .green {
+        color: green;
+      }
+    </style>
+  </head>
+  <body>
+    <h1 class="<?php echo $class ?>"><?php echo $message ?></h1>
+  </body>
+</html>
